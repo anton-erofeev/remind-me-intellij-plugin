08cfbe850162a806e9e17baefc8d752816ddb7d9
GitHub Action <action@github.com>
2023-06-19 13:06:57 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "remind-me-intellij-plugin"
--- src/main/kotlin/com/github/antonerofeev/remindmeintellijplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/antonerofeev/remindmeintellijplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.antonerofeev.remindmeintellijplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
